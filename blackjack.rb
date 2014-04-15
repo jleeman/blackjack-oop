89429f593488fb325627a108cd33f162d475b416
Josh Leeman <joshleeman@gmail.com>
2014-04-15 00:44:24 -0400

    sigh

--- blackjack.rb
+++ blackjack.rb
@@ -1,169 +1,141 @@
-# super class - both dealer & players share name / scores / status method commenting out for now, not sure this is necessary
-# class Gambler
-#   attr_accessor :name, :score, :status
+require 'pry'
 
-#   def initialize n, s
-#     @name = n
-#     @score = s
-#   end
+# super class for player/dealer, put methods/variables they share in common here
+class CasinoPerson
+  attr_accessor :name, :hand
 
-#   # takes current score as input - returns blackjack/bust as string or current score if neither, kinda sloppy maybe rethink this
-#   def status s
-#     status = s
-#     if s == 21
-#       status = 'blackjack'
-#     elsif s > 21
-#       status = 'bust'
-#     end
-#     status
-#   end
-# end
-
-class Player 
-  attr_accessor :name, :hand, :score
-  
-  def initialize n
-    @name = n
+  def initialize (name)
+    @name = name
     @hand = []
-    @score = 0
   end
 
-  # pass in player, return updated hand value
-  # def round p
-  #   hit = true
-  #   while hit
-  #     puts "You have #{p.@hand}, your current score is "
-  #   end
-  # end
-end
-
-class Dealer < Player
-  attr_accessor :deck, :hand
-
-  # speaks, takes what to speak string command as input
-  def speak s
-    case s 
-      when 'num_players'
-        words = 'How many players will there be?'
-      when 'welcome'
-        words = 'Welcome to the casino!!!'
-      when 'name'
-        words = 'What is your name?'
-      when 'play'
-        words = 'Shoud we play another game?'
-      when 'hit'
-        words = 'Do you want to hit or not?'
+  # return hand value as integer at any time
+  def hand_value (hand)
+    hand_value = 0
+    hand.each do |card|
+      case card.value
+        when /[2-9]|[1][0]/ 
+          hand_value += card.value.to_i 
+        when /[JQK]/
+          hand_value += 10
+        when "A"
+          hand_value += 11
+          if hand_value > 21
+            hand_value -= 10 
+          end 
+      end
     end
-    words
+    hand_value
   end
+
+  # boolean for blackjack
+  def blackjack? (hand_value)
+    if hand_value == 21
+      return true
+    else
+      return false
+    end
+  end
+
+  # boolean for bust
+  def bust? (hand_value)
+    if hand_value > 21
+      return true
+    else
+      return false
+    end
+  end
+
 end
 
+class Player < CasinoPerson
 
+  def round (player, deck)
+    hand_value = player.hand_value(player.hand)
+    puts "You have been dealt #{player.hand}. Your current hand value is #{hand_value}."
+    if player.blackjack?(hand_value) != true 
+      puts "Would you like to hit or stay? hit/stay response only."
+      hit_stay = gets.chomp.downcase
+      while hit_stay == "hit"
+        card = deck.cards.pop
+        player.hand << card
+        hand_value = player.hand_value(player.hand)
+        if player.blackjack?(hand_value) || player.bust?(hand_value)
+          break
+        else
+          puts "You have been dealt #{card.value}. Your current hand value is #{hand_value}."
+          puts "Would you like to hit or stay? hit/stay response only."
+          hit_stay = gets.chomp.downcase
+        end
+      end        
+    end
+    hand_value
+  end
+
+end
+
+class Dealer < CasinoPerson
+
+  def get_name
+    puts "Welcome to the casino!"
+    puts "What's your name?"
+    player_name = gets.chomp
+  end
+
+end
 
 class Deck
-  attr_accessor :cards, :num_decks
-  # set up suits & values as constants, they should never change
-  SUITS = ['S', 'C', 'D', 'H']
-  VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-
-  # takes number of decks as input
-  def initialize n
-    @cards = []
-    @num_decks = n
-    @num_decks.times do 
-      SUITS.each do |s|
-        VALUES.each do |v|
-          @cards << Card.new(s,v)
-        end
-      end
-    end
-  end
+  attr_accessor :cards
   
-  # returns shuffled array of cards, no matter how many cards there are
-  def shuffle 
-    @cards.shuffle
-  end
-
-  def first_deal
-    2.times do
-      @players.each do |x|
-        @players[x].hand <<  @deck.pop
-        puts "#{@players[x].hand}"
+  def initialize
+    suits = ['S', 'C', 'D', 'H']
+    values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    @cards = []
+    suits.each do |s|
+      values.each do |v|
+        @cards << Card.new(s, v)
       end
-      @dealer.hand << @deck.pop
-      puts "#{@dealer.hand}"
     end
-  end
-
-  #takes any number cards to deal as input, returns the dealt cards
-  def deal n
-    @dealt_cards = []
-    n.times {@dealt_cards << @cards.pop}
-    @dealt_cards
+    @cards.shuffle!
   end
 end
 
 class Card 
   attr_accessor :suit, :value
 
-  def initialize s, v
+  def initialize(s, v)
     @suit = s
     @value = v
-  end
-
-  def display_card
-    "#{suit}, #{value}"
   end
 end
 
 # this is the "engine" that runs blackjack game
 class Blackjack
-  attr_accessor :dealer, :players, :deck
 
   def initialize 
-    @dealer = Dealer.new('Dealer')
-    @players = []
-    # can change how many decks get shuffled here, 4 vegas style
-    @deck = Deck.new(4)
-    run
+    dealer = Dealer.new("Dealer")
+    player_name = dealer.get_name
+    player = Player.new("#{player_name}")
+    deck = Deck.new
+    play_game(dealer, player, deck)
+    # card_value = @deck.cards[1].value
+    # puts "#{card_value}"
   end
 
-  # after initialization, this runs one full game, use recursion to y/n run again, idea of popping off players if they don't want to play
-  # output here, who won? 
-  def run
-    @players = store_players(@players)
-    first_deal(@deck.cards)
-  end
-
-  def store_players arr 
-    puts @dealer.speak('num_players')
-    num_players = gets.chomp.to_i
-    player_id = 1
-    num_players.times do
-      puts "Player #{player_id}, " + @dealer.speak('name')
-      name = gets.chomp
-      # create player and push to players array
-      arr << Player.new(name)
-      player_id += 1
+  def play_game (dealer, player, deck)
+    # initial deal
+    2.times do
+      player.hand << deck.cards.pop
+      dealer.hand << deck.cards.pop
     end
-    puts arr[0].name
-    puts arr[1].name
-    puts arr[2].name
-    arr
-  end
-
-
-  # players take turns
-  def player_rounds
-    @players.each do |p| 
-     p.round
-    end
+    player_hand = player.hand
+    dealer_hand = dealer.hand
+    puts "Player hand is #{player_hand}"
+    puts "Dealer hand is #{dealer_hand}"
+    player_hand_value = player.round(player, deck)
+    puts "#{player_hand_value}"
   end
 end
 
 game = Blackjack.new
-
-
-
-
 
